c1a75c5163794e4be5ae4955f839243b5e8341aa
Chukwu <e.c.chukwu@tue.nl>
2024-07-25 18:18:04 +0200

    added classifiers

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from data_processing.file_operations import process_directory
 from data_processing.glucose_processing import process_glucose_data
 from data_processing.interpolation import interpolate_glucose_levels
 from data_processing.steps_processing import process_all_patients
-from classification.classifiers import train_classifiers
+from classification.classifiers import steps_classifiers, sktime_classifiers, LSTM_classifier
+from data_merging import merge_patient_data
 
 def main():
     selected_dir = 'Selected_Patients'
@@ -10,7 +11,9 @@ def main():
     process_glucose_data(selected_dir)
     interpolate_glucose_levels(selected_dir)
     process_all_patients(selected_dir)
-    train_classifiers()
+    merge_patient_data(selected_dir)
+    LSTM_classifier()
+    sktime_classifiers()
 
 if __name__ == "__main__":
     main()
